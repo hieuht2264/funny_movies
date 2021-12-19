8b9b7e8da73bbda92ff89a6b66b7734c801a0236
hieu hoang <hieu.h@cs.ventures>
2021-12-19 21:44:50 +0700

    apply rubocop

--- app/decorators/video_decorator.rb
+++ app/decorators/video_decorator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class VideoDecorator < Draper::Decorator
   delegate_all
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   def video_embed_url_generator(youtube_url)
-    youtube_id = youtube_url.split("=").last
+    youtube_id = youtube_url.split('=').last
     content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}", width: 500, height: 280)
   end
 end
